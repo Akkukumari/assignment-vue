ac15647b14b5ac8c9276872799b7bae80a7e0e30
Akkukumari <akanksha28sbg@gmail.com>
2024-01-18 09:30:14 +0530

    done

--- projectvue/src/views/HomeView.vue
+++ projectvue/src/views/HomeView.vue
@@ -1,13 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome :data="childData" />
-  </main>
+  <div class="home">
+    <TheWelcome :prop1="value1" :prop2="value2" :prop3="value3" />
+  </div>
 </template>
+
 <script>
-const childData = 'Akansha';
+import TheWelcome from '../components/TheWelcome.vue';
+
+export default {
+  components: {
+    TheWelcome,
+  },
+  data() {
+    return {
+      value1: 'Akansha',
+      value2: 24,
+      value3: { object: 'masai school' },
+    };
+  },
+};
 </script>
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
 
