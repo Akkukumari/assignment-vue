b61c665834492663eaed896d2bb2498792609c00
Akkukumari <akanksha28sbg@gmail.com>
2024-01-18 13:43:41 +0530

    done

--- projectvue-akansha - Copy/src/views/HomeView.vue
+++ projectvue-akansha - Copy/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="parent">
     <TheWelcome :prop1="value1" :prop2="value2" :prop3="value3" />
   </div>
 </template>
@@ -13,20 +13,11 @@ export default {
   },
   data() {
     return {
-      value1: 'Akansha',
-      value2: 24,
-      value3: { object: 'masai school' },
+      value1: 'Data for prop1',
+      value2: 42,
+      value3: { object: 'property' },
     };
   },
 };
 </script>
-<style>
-@media (min-width: 1024px) {
-  .home {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
 
